0cfa896ac57c317db0059f1df989b9159273c5e1
MrJanHorak <janny_b_goode@hotmail.com>
2021-10-25 20:02:10 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,30 @@
+//import modules
+
+import express from 'express'
+
+// create Express App
+
+const app = express()
+
+// Configure the app (app.set)
+
+
+
+// Mount Middleware (app.use)
+
+
+
+// Mount routes
+
+app.get('/', function(req, res){
+  res.send('<h1>hello, friend</h1>')
+})
+
+
+// Tell the app to listen on port 3000
+
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000")
+})
+
